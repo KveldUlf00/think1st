e3aba79051781d083fb8b862d32731041cbd5cfb
Blazej Raducki <raduckib@gmail.com>
2024-07-21 16:30:35 +0200

    feat: download all packages that will be propably needed

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import react from "eslint-plugin-react";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...fixupConfigRules(compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+)), {
+    plugins: {
+        "@typescript-eslint": fixupPluginRules(typescriptEslint),
+        react: fixupPluginRules(react),
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...globals.browser,
+        },
+
+        parser: tsParser,
+    },
+
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+
+    rules: {},
+}];
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles/index.scss';
+import App from './pages/App';
+import { SnackbarProvider } from 'notistack';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <SnackbarProvider
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      autoHideDuration={3000}
+      preventDuplicate
+    >
+      <App />
+    </SnackbarProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
